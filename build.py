b2a8f00eb9275e5121279e10a5638a8bc75ca793
lavirlifiliol <adam.hrbac@gmail.com>
2021-03-28 21:24:44 +0200

    u

--- build.py
+++ build.py
@@ -8,15 +8,14 @@ from pathlib import Path
 from subprocess import check_call as call
 
 arcade_path = pkgutil.find_loader('arcade')
-if arcade_path is not None:
-    arcade_path = arcade_path.path
-    hook_path = Path(arcade_path).parent / '__pyinstaller' / 'hook-arcade.py'
-    assert hook_path.exists()
-    # the arcade hook is incorrectly setup to include chipmunk.dll/.so which doesn't exist
-    with hook_path.open('a') as f:
-        f.write('\nbinaries = []\n')
+arcade_path = arcade_path.path
+hook_path = Path(arcade_path).parent / '__pyinstaller' / 'hook-arcade.py'
+assert hook_path.exists()
+# the arcade hook is incorrectly setup to include chipmunk.dll/.so which doesn't exist
+with hook_path.open('a') as f:
+    f.write('\nbinaries = []\n')
 
-    print('updated arcade pyinstaller hook')
+print('updated arcade pyinstaller hook')
 
 
 call(['git', 'clone', 'https://github.com/pyinstaller/pyinstaller.git'])

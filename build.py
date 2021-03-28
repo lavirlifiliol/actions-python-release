589c2916b7461f7cfb71be423006adb8bcc6dfe8
lavirlifiliol <adam.hrbac@gmail.com>
2021-03-28 14:14:16 +0200

    Add buld.py
    a hopefully portable buildscript

---
+++ build.py
@@ -0,0 +1,37 @@
+#!//usr/bin/env python
+"""
+fixes the arcade pyinstaller hook and builds a onefile executable in a cross platform way
+"""
+from os import chdir, mkdir
+from pathlib import Path
+from subprocess import check_call as call
+
+from arcade import __file__ as arcade_path
+
+hook_path = Path(arcade_path).parent / '__pyinstaller' / 'hook-arcade.py'
+assert hook_path.exists()
+
+# the arcade hook is incorrectly setup to include chipmunk.dll/.so which doesn't exist
+with hook_path.open('a') as f:
+    f.write('\nbinaries = []\n')
+
+print('updated arcade pyinstaller hook')
+
+
+call(['git', 'clone', 'https://github.com/pyinstaller/pyinstaller.git'])
+chdir('pyinstaller')
+chdir('bootloader')
+call(['python', 'waf', 'all'])
+
+print('rebuilt pyinstaller bootloader')
+
+chdir('..')
+chdir('..')
+call(['pip', 'install', '-e', 'pyinstaller'])
+
+print('installed pyinstaller')
+
+call(['pyinstaller', '--onefile', 'main.pyw'])
+
+print('created dist/main.exe')
+print('done')

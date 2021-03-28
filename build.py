da52a19e8788299760c98a58ae0f6367b03c85cb
lavirlifiliol <adam.hrbac@gmail.com>
2021-03-28 22:26:21 +0200

    tete

--- build.py
+++ build.py
@@ -2,8 +2,8 @@
 """
 fixes the arcade pyinstaller hook and builds a onefile executable in a cross platform way
 """
-from os import chdir, mkdir
 import pkgutil
+from os import chdir
 from pathlib import Path
 from subprocess import check_call as call
 
@@ -16,7 +16,6 @@ with hook_path.open('a') as f:
     f.write('\nbinaries = []\n')
 
 print('updated arcade pyinstaller hook')
-
 
 call(['git', 'clone', 'https://github.com/pyinstaller/pyinstaller.git'])
 chdir('pyinstaller')
@@ -31,7 +30,7 @@ call(['pip', 'install', '-e', 'pyinstaller'])
 
 print('installed pyinstaller')
 
-call(['pyinstaller', '--onefile', 'main.pyw'])
+call(['pyinstaller', '--onefile', '--hidden-import', 'arcade', 'main.pyw'])
 
 print('created dist')
 print('done')

03fc1c65c211b877e64ee36c0d9220662ba15952
lavirlifiliol <adam.hrbac@gmail.com>
2021-03-29 13:45:48 +0200

    Run pyinstaller as a module

--- build.py
+++ build.py
@@ -5,7 +5,6 @@ fixes the arcade pyinstaller hook and builds a onefile executable in a cross pla
 from os import chdir
 from subprocess import check_call as call
 
-
 call(['git', 'clone', 'https://github.com/pyinstaller/pyinstaller.git'])
 chdir('pyinstaller')
 call(['git', 'checkout', 'tags/v4.2'])
@@ -20,7 +19,7 @@ call(['pip', 'install', '-e', 'pyinstaller'])
 
 print('installed pyinstaller')
 
-call(['pyinstaller', '--onefile', '--hidden-import', 'arcade', 'main.pyw'])
+call(['python3.9', '-m', 'PyInstaller', '--onefile', 'main.pyw'])
 
 print('created dist')
 print('done')

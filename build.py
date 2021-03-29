94f47467a15ea81be7d3e71f582e68822e0e8b27
lavirlifiliol <adam.hrbac@gmail.com>
2021-03-29 13:38:28 +0200

    use a released version of pyinstaller

--- build.py
+++ build.py
@@ -2,22 +2,12 @@
 """
 fixes the arcade pyinstaller hook and builds a onefile executable in a cross platform way
 """
-import pkgutil
 from os import chdir
-from pathlib import Path
 from subprocess import check_call as call
 
-arcade_path = pkgutil.find_loader('arcade')
-arcade_path = arcade_path.path
-hook_path = Path(arcade_path).parent / '__pyinstaller' / 'hook-arcade.py'
-assert hook_path.exists()
-# the arcade hook is incorrectly setup to include chipmunk.dll/.so which doesn't exist
-with hook_path.open('a') as f:
-    f.write('\nbinaries = []\n')
-
-print('updated arcade pyinstaller hook')
 
 call(['git', 'clone', 'https://github.com/pyinstaller/pyinstaller.git'])
+call(['git', 'checkout', 'tags/v4.2'])
 chdir('pyinstaller')
 chdir('bootloader')
 call(['python', 'waf', 'all'])
